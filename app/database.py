97435aad2e36a47984c7c1bf287048f83ddf5b98
anonode <olivarririchard@gmail.com>
2025-03-19 11:01:02 -0500

    work

--- app/database.py
+++ app/database.py
@@ -1,4 +1,4 @@
-def createUser(mysql, username, password) -> bool:
+def createUser(mysql, email, username, password) -> bool:
     """Register new user into the database
 
     Args:
@@ -9,6 +9,11 @@ def createUser(mysql, username, password) -> bool:
     Returns:
         bool: True if user was created in database, False otherwise.
     """
+
+    # TO-DO: hash passwords before inserting into database
+    cursor = mysql.connection.cursor()
+    query = f"INSERT INTO users (username, email, password) VALUES ({username}, {email}, {password});"
+    cursor.execute(query)
     return True
 def userLogin(mysql, username, password) -> bool:
     return True
