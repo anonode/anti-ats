89935991a4241a90fbaa04d7a5b675f6fcef04d5
anonode <olivarririchard@gmail.com>
2025-03-27 15:41:56 -0500

    db

--- db.sql
+++ db.sql
@@ -3,6 +3,8 @@ use swe;
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
+    fname varchar(50) not null,
+    lname varchar(50) not null,
     email VARCHAR(100) NOT NULL,
     password VARCHAR(255) NOT NULL,
     UNIQUE(email),
