22bcbdd277c045d11a9279af772c501ead723b56
anonode <olivarririchard@gmail.com>
2025-04-01 16:31:43 -0500

    remove dependancy

--- app/app.py
+++ app/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_mysqldb import MySQL
-from datetime import datetime, timedelta
 from config import Config
 import os
 
