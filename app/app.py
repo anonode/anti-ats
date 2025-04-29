10778f70be96e828ea4d7338e7338c999657ba3c
anonode <olivarririchard@gmail.com>
2025-04-29 11:32:14 -0500

    work

--- app/app.py
+++ app/app.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask import Flask, render_template, request, redirect, url_for, flash, session, current_app, request, abort
+import requests
+import secrets # for OAuth
+from urllib.parse import urlencode
 from flask_dance.contrib.google import make_google_blueprint, google
+from flask_login import LoginManager, UserMixin, login_user, logout_user, current_user
 from werkzeug.utils import secure_filename
 import os, time
 # our stuff
@@ -14,14 +18,32 @@ upload_path = "/home/anti-ats/submissions/" # adjust this later
 app.secret_key = app.config["SECRET_KEY"] # sign session cookies
 mysql = MySQL(app)
 
-google_bp = make_google_blueprint(
+app.config['OAUTH2_PROVIDERS'] = {
+    # Google OAuth 2.0 documentation:
+    # https://developers.google.com/identity/protocols/oauth2/web-server#httprest
+    'google': {
+        'client_id': os.environ.get("GOOGLE_OAUTH_CLIENT_ID"),
+        'client_secret': os.environ.get("GOOGLE_OAUTH_CLIENT_SECRET"),
+        'authorize_url': 'https://accounts.google.com/o/oauth2/auth',
+        'token_url': 'https://accounts.google.com/o/oauth2/token',
+        'userinfo': {
+            'url': 'https://www.googleapis.com/oauth2/v3/userinfo',
+            'email': lambda json: json['email'],
+        },
+        'scopes': ['https://www.googleapis.com/auth/userinfo.email'],
+    }
+}
+
+login = LoginManager(app)
+login.login_view =  'home'
+'''google_bp = make_google_blueprint(
     client_id=app.config["GOOGLE_OAUTH_CLIENT_ID"],
     client_secret=app.config["GOOGLE_OAUTH_CLIENT_SECRET"],
     redirect_to="google_login",
     scope=["profile", "email"]
 )
 app.register_blueprint(google_bp, url_prefix="/login")
-
+'''
 @app.route("/", methods=["GET", "POST"])
 def home():
     if "user_id" not in session:
@@ -186,6 +208,99 @@ def register():
     
     
     return render_template("register.html") # redirect to register.html with HTTP Redirect and implicit GET request
+
+@app.route('/authorize/<provider>')
+def oauth2_authorize(provider):
+    #if not current_user.is_anonymous:
+     #   return redirect(url_for('index'))
+
+    provider_data = current_app.config['OAUTH2_PROVIDERS'].get(provider)
+    if provider_data is None:
+        abort(404)
+
+    # generate a random string for the state parameter
+    session['oauth2_state'] = secrets.token_urlsafe(16)
+
+    # create a query string with all the OAuth2 parameters
+    qs = urlencode({
+        'client_id': provider_data['client_id'],
+        'redirect_uri': url_for('oauth2_callback', provider=provider,
+                                _external=True),
+        'response_type': 'code',
+        'scope': ' '.join(provider_data['scopes']),
+        'state': session['oauth2_state'],
+    })
+
+    # redirect the user to the OAuth2 provider authorization URL
+    return redirect(provider_data['authorize_url'] + '?' + qs)
+
+@app.route('/callback/<provider>')
+def oauth2_callback(provider):
+    if not current_user.is_anonymous:
+        return redirect(url_for('index'))
+
+    provider_data = current_app.config['OAUTH2_PROVIDERS'].get(provider)
+    if provider_data is None:
+        abort(404)
+
+    # if there was an authentication error, flash the error messages and exit
+    if 'error' in request.args:
+        for k, v in request.args.items():
+            if k.startswith('error'):
+                flash(f'{k}: {v}')
+        return redirect(url_for('index'))
+
+    # make sure that the state parameter matches the one we created in the
+    # authorization request
+    if request.args['state'] != session.get('oauth2_state'):
+        abort(401)
+
+    # make sure that the authorization code is present
+    if 'code' not in request.args:
+        abort(401)
+
+    # exchange the authorization code for an access token
+    response = requests.post(provider_data['token_url'], data={
+        'client_id': provider_data['client_id'],
+        'client_secret': provider_data['client_secret'],
+        'code': request.args['code'],
+        'grant_type': 'authorization_code',
+        'redirect_uri': url_for('oauth2_callback', provider=provider,
+                                _external=True),
+    }, headers={'Accept': 'application/json'})
+    if response.status_code != 200:
+        abort(401)
+    oauth2_token = response.json().get('access_token')
+    if not oauth2_token:
+        abort(401)
+
+    # use the access token to get the user's email address
+    response = requests.get(provider_data['userinfo']['url'], headers={
+        'Authorization': 'Bearer ' + oauth2_token,
+        'Accept': 'application/json',
+    })
+    if response.status_code != 200:
+        abort(401)
+    email = provider_data['userinfo']['email'](response.json())
+
+    # find or create the user in the database
+    # Retrieve the user by email
+    user = get_user_by_email(email)
+
+    # If user doesn't exist, create a new one
+    if not user:
+        # Create a new user with the provided email and username
+        username = email.split('@')[0]  # using the part before "@" in email as the username
+        create_user(username=username, password=None, email=email)
+        # Re-fetch the user to get the newly created one
+        user = get_user_by_email(email)
+
+    # Now `user` is either the existing user or the newly created one
+    user_id = user[0]  # Assuming the user is a tuple where the first element is user_id
+    session["user_id"] = user_id
+    session["username"] = username
+    flash("Logged in successfully with Google", "success")
+    return redirect(url_for("home"))
 
 
 ######## Obligatory #########
